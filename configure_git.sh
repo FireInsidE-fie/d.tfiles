b3c0984ca525d593bb057747fac6320d4ae3d4c6
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-05-06 11:30:25 +0200

    update configure_git to not add duplicate settings

--- configure_git.sh
+++ configure_git.sh
@@ -1,8 +1,12 @@
 #/bin/bash
 
+git config --unset user.name
+git config --unset user.email
+git config --unset --global init.defaultbranch
+
 git config user.name "Aisling Fontaine"
 git config user.email "aisling.fontaine@protonmail.com"
 git config --global pull.rebase true
-git config init.defaultbranch "main"
+git config --global init.defaultbranch "main"
 
 echo "[!] - Configuring done! Don't forget to setup your ssh key(s) <3"
