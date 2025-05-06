dd604c590e5bea575a3cbb9c3c3bae55b1115e06
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-05-06 11:27:39 +0200

    add script to configure my usual git settings on a given machine

---
+++ configure_git.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+git config user.name "Aisling Fontaine"
+git config user.email "aisling.fontaine@protonmail.com"
+git config --global pull.rebase true
+git config init.defaultbranch "main"
+
+echo "[!] - Configuring done! Don't forget to setup your ssh key(s) <3"
