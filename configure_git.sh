18e80052d0c7b7154dd9a230375694581513262f
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-09-24 11:51:51 +0200

    zed: add toggle to show inlay hints / git: set name/email global

--- configure_git.sh
+++ configure_git.sh
@@ -4,8 +4,8 @@ git config --unset user.name
 git config --unset user.email
 git config --unset --global init.defaultbranch
 
-git config user.name "Aisling Fontaine"
-git config user.email "aisling.fontaine@protonmail.com"
+git config --global user.name "Aisling Fontaine"
+git config --global user.email "aisling.fontaine@protonmail.com"
 git config --global pull.rebase true
 git config --global init.defaultbranch "main"
 
